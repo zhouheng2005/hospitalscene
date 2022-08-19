d92ddff5cc669f21860878f663d418c643b7887d
周珩 <zhouheng@yfkjqh.com>
2022-08-19 11:30:13 +0800

    上传原生代码

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<web-view class="web" src="https://vr.congrongtech.cn/websocket/#/"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
